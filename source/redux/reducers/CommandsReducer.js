665fea2c08ef68b88fed454816ec3e839b3b46bd
shin2005 <cieloeste0303@gmail.com>
2017-01-07 13:24:36 +0900

    revised command tab

--- source/redux/reducers/CommandsReducer.js
+++ source/redux/reducers/CommandsReducer.js
@@ -3,7 +3,7 @@ const defaultState = {
     columns: ['#', 'Command', 'description'],
     rows: [
       ['1', '/gamemode', '게임모드 (크리에티브, 서바이벌, 어드벤처, 관전) 중 하나로 바꾼다.'],
-      ['2', '/spawnpoint', '스폰포인트 ()'],
+      ['2', '/spawnpoint', '스폰포인트 (죽으면 현 위치에 스폰이 되게 하는 커맨드)'],
       ['3', '/help [CommandName]', 'Provides more information on the given command.']
     ]
   }
