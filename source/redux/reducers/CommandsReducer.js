49218aae20d4cf6983588d8d44b28909ead219f5
shin2005 <cieloeste0303@gmail.com>
2017-01-14 13:32:09 +0900

    added line

--- source/redux/reducers/CommandsReducer.js
+++ source/redux/reducers/CommandsReducer.js
@@ -4,7 +4,8 @@ const defaultState = {
     rows: [
       ['1', '/gamemode', '게임모드 (크리에티브, 서바이벌, 어드벤처, 관전) 중 하나로 바꾼다.'],
       ['2', '/spawnpoint', '스폰포인트 (죽으면 현 위치에 스폰이 되게 하는 커맨드)'],
-      ['3', '/help [CommandName]', 'Provides more information on the given command.']
+      ['3', '/help [CommandName]', 'Provides more information on the given command.'],
+      ['4', '/give', 'Gives you a specific item(including things not in your inventory)']
     ]
   }
 };
